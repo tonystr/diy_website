e1376ce4ddabfb9c8f048b5964deb94d5100010e
TonyStr <tony.stroemsnaes@gmail.com>
2020-11-26 15:17:25 +0100

    lang select

--- files/languge_switch.js
+++ files/languge_switch.js
@@ -1,15 +1,21 @@
 
 function renderLanguageSwitch() {
     const languageElm = document.querySelector('.fixed-header .language');
+    const href = languageElm.href;
 
     const selectElm = document.createElement('select');
     selectElm.classList.add('language-select');
-    selectElm.innerHTML = `
+    selectElm.innerHTML = window.location.href.includes('/no/') ? `
+        <option>Norsk</option>
         <option>English</option>
-        <option>Norwegian</option>
+    ` : `
+        <option>English</option>
+        <option>Norsk</option>
     `;
 
-    console.log('hrtef', languageElm.href);
+    selectElm.addEventListener('change', () => {
+        window.location.href = href;
+    });
 
     languageElm.parentElement.classList.add('select-inside');
     languageElm.parentElement.insertBefore(selectElm, languageElm);

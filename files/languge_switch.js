4fa035b0b57055fe4264b095620e711dd898d4c5
TonyStr <tony.stroemsnaes@gmail.com>
2020-11-26 15:26:18 +0100

    Polish lang switch & label

--- files/languge_switch.js
+++ files/languge_switch.js
@@ -3,21 +3,30 @@ function renderLanguageSwitch() {
     const languageElm = document.querySelector('.fixed-header .language');
     const href = languageElm.href;
 
-    const selectElm = document.createElement('select');
-    selectElm.classList.add('language-select');
-    selectElm.innerHTML = window.location.href.includes('/no/') ? `
-        <option>Norsk</option>
-        <option>English</option>
-    ` : `
-        <option>English</option>
-        <option>Norsk</option>
+    const divElm = document.createElement('div');
+    const nor = window.location.href.includes('/no/');
+
+    divElm.innerHTML = `
+        <label class="visually-hidden" for="language-select">${nor ? 'Språk: ' : 'Language: '}</label>
+        <select class="language-select" id="language-select">
+            ${nor ? `
+                <option>Norsk</option>
+                <option>English</option>
+            ` : `
+                <option>English</option>
+                <option>Norsk</option>
+            `}
+        </select>
     `;
 
-    selectElm.addEventListener('change', () => {
+    divElm.children[1].addEventListener('change', () => {
         window.location.href = href;
     });
 
+    // <label class="visually-hidden" for="source-search">Search sources:</label>
+
+
     languageElm.parentElement.classList.add('select-inside');
-    languageElm.parentElement.insertBefore(selectElm, languageElm);
+    languageElm.parentElement.insertBefore(divElm, languageElm);
     languageElm.parentElement.removeChild(languageElm);
 }

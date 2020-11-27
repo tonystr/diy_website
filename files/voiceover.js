3b9068e5294daae3c850e8c43ee68224e42eccab
TonyStr <tony.stroemsnaes@gmail.com>
2020-11-27 15:19:51 +0100

    Add button hint

--- files/voiceover.js
+++ files/voiceover.js
@@ -10,6 +10,7 @@ function renderVoiceover() {
             <span class="visually-hidden">${NOR ? 'Les høyt' : 'Play voiceover'}</span>
             <img src="./images/audio.png" alt="Sound icon">
         `;
+        buttonElm.setAttribute('title', NOR ? 'Les høyt' : 'Play voiceover');
         buttonElm.addEventListener('click', () => {
             buttonElm.classList.add('visually-hidden');
             audioElm.classList.remove('visually-hidden');
